ad8c82050be0f19e9eeb3f198e64f762612e85de
lguardado <lucas.guardado@gmail.com>
2020-10-13 08:53:32 -0300

    fix typo in index

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,31 @@
-import { armyCreator } from "./creators/armyCreator";
+import { advancedArmyCreator } from "./creators/armyCreator";
 import { UnitTypesEnum } from "./constants/armyConstants";
 import { civilizationCreator } from "./creators/civilizationCreator";
 import { CivilizationTypesEnum } from "./constants/civilizationConstants";
 
 const english = civilizationCreator(CivilizationTypesEnum.english);
-english.addArmy(armyCreator(english.getType()));
+english.addArmy(advancedArmyCreator(english.getType()));
 const byzantine = civilizationCreator(CivilizationTypesEnum.byzantine);
-byzantine.addArmy(armyCreator(byzantine.getType()));
+byzantine.addArmy(advancedArmyCreator(byzantine.getType()));
 const englishFirstInfantry = english.armies[0];
 const byzantineFirstInfantry = byzantine.armies[0];
 
 // DESCOMENTAR PARA ENTRENAR UNIDADES
-// byzantineFirstInfantry.trainUnit(UnitTypesEnum.knight);
-// byzantineFirstInfantry.trainUnit(UnitTypesEnum.archer);
+byzantineFirstInfantry.trainUnit(UnitTypesEnum.knight);
+byzantineFirstInfantry.trainUnit(UnitTypesEnum.archer);
 
 // DESCOMENTAR PARA TESTEAR BATALLA!
-// englishFirstInfantry.attack(byzantine.armies[0]);
+englishFirstInfantry.attack(byzantine.armies[0]);
 
 // DESCOMENTAR PARA TESTEAR EMPATE!
-// englishFirstInfantry.attack(english.armies[0]);
+englishFirstInfantry.attack(english.armies[0]);
 
 // DESCOMENTAR PARA TESTEAR HISTORIAL!
-// console.log(englishFirstInfantry.getHistory());
+console.log(englishFirstInfantry.getHistory());
 
 // DESCOMENTAR PARA TESTEAR UPGRADE
-// englishFirstInfantry.upgradeUnit(UnitTypesEnum.archer);
-// englishFirstInfantry.upgradeUnit(UnitTypesEnum.soldier);
+englishFirstInfantry.upgradeUnit(UnitTypesEnum.archer);
+englishFirstInfantry.upgradeUnit(UnitTypesEnum.soldier);
 
 // DESCOMENTAR PARA TESTEAR UPGRADE FALLIDO
-// englishFirstInfantry.upgradeUnit(UnitTypesEnum.knight);
+englishFirstInfantry.upgradeUnit(UnitTypesEnum.knight);
